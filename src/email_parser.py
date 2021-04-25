f86e2224e0c98c4ee880cc57f480452c4e8849f6
Olatunde Sodiq <olatundesodiq@gmail.com>
2021-04-25 07:14:58 +0100

    solution to regular expression

--- src/email_parser.py
+++ src/email_parser.py
@@ -1,2 +1,56 @@
 # Write you email parser code here ...
 
+# Import all required modules
+import re
+
+
+class EmailParser:
+    # regex pattern that validate email
+    pattern = r'^([a-zA-Z])([a-zA-Z0-9+]+)@([a-zA-Z])([a-zA-Z0-9]+)\.com$'
+    keys = ['username', 'domain']
+
+    @classmethod
+    def parse(cls, email):
+        ''' parse email and return a dictionary of username and domain if valid and return None otherwise'''
+
+        if not type(email) is str:
+            return None
+
+        pattern = re.compile(cls.pattern)
+
+        search_email = pattern.search(email)
+
+        if search_email:
+            grouped_email = search_email.group() # 'ola@gmail.com'
+            splittee_email = grouped_email.split('@') # [ola, gmail.com]
+
+            return cls.convert_to_dict(cls.keys, splittee_email)
+
+        return search_email
+
+        # Add implementation here ...
+    def convert_to_dict(first_list, second_list):
+        '''
+          Takes in two lists as params and return them as a dictionary.
+
+          Parameters:
+            first_list (list []): first_list to serve as keys  
+            second_list (list []): second_list to serve as values
+
+          Returns:
+            convert_to_dict(list, list): return a dictionary of two lists 
+        '''
+        return dict(zip(first_list, second_list))
+
+
+# -----------------------------------------------------
+# EXPERIMENTS
+# -----------------------------------------------------
+# Ensure to clear out codes below when you are done
+# with experimentations
+# -----------------------------------------------------
+print('\n>>>> Experiment - EmailParser\n')
+
+# result = EmailParser.parse('name@gmail.com')
+# print(result)
+print('')
--- tests/test_email_parser.py
+++ tests/test_email_parser.py
@@ -1,12 +1,47 @@
+# Import all required modules
+from unittest import TestCase
+from src.email_parser import EmailParser
 
 
 class TestEmailParser(TestCase):
-  pass # write all test here
 
+    def setUp(self) -> None:
+        self.exec = lambda email: EmailParser.parse(email)
 
+    def test_parse_simple_valid_email(self):
 
+        result = {'username': 'ola', 'domain': 'gmail.com'}
 
+        self.assertEqual(self.exec('ola@gmail.com'), result)
 
+    def test_parser_with_invalid_email(self):
+        self.assertEqual(self.exec('olat.gmail.com'), None)
+        self.assertEqual(self.exec('www.olatgmail.com'), None)
 
-if __name__== 'main':
-  unittest.main()
+    def test_parser_with_username_with_plus(self):
+        result = {'username': 'ola+sodiq', 'domain': 'yahoo.com'}
+        self.assertEqual(self.exec('ola+sodiq@yahoo.com'), result)
+
+    def test_parser_with_characters(self):
+        self.assertEqual(self.exec('ola=sodiq@gmail.com'), None)
+        self.assertEqual(self.exec('ola_sodiq@gmail.com'), None)
+
+    def test_parser_with_domain_endwith_number(self):
+        result = {'username': 'ola+sodiq', 'domain': 'yahoo33.com'}
+        self.assertEqual(self.exec('ola+sodiq@yahoo33.com'), result)
+
+    def test_parser_with_random_data_types(self):
+        self.assertEqual(self.exec(''), None)
+        self.assertEqual(self.exec(123), None)
+        self.assertEqual(self.exec(['olatunde@gmail.com']), None)
+
+    def test_parser_with_email_start_with_number(self):
+        self.assertEqual(self.exec('9ola@gmail.com'), None)
+        self.assertEqual(self.exec('ola@6gmail.com'), None)
+
+    def test_parser_with_email_not_end_with_dotcom(self):
+        self.assertEqual(self.exec('ola@gmail.co'), None)
+        self.assertEqual(self.exec('ola@gmail.co.uk'), None)
+
+    def tearDown(self) -> None:
+        self.exec = None
